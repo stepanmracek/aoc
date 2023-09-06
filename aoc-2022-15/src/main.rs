23d8f68762fece8e0cce82ab2414c34dcd55796f
Štěpán Mráček <stepan.mracek@gmail.com>
2023-09-06 09:20:07 +0200

    perf improvement

--- aoc-2022-15/src/main.rs
+++ aoc-2022-15/src/main.rs
@@ -1,5 +1,7 @@
-use kdam::tqdm;
-use std::str::FromStr;
+use itertools::Itertools;
+use std::cmp::Ordering;
+use std::ops::RangeInclusive;
+use std::{collections::HashSet, str::FromStr};
 
 #[derive(Debug)]
 struct Coord {
@@ -45,7 +47,7 @@ fn manhattan_distance(first: &Coord, second: &Coord) -> i32 {
     (first.x.abs_diff(second.x) + first.y.abs_diff(second.y)) as i32
 }
 
-fn intersect(sensor_beacon: &SensorBeacon, y: i32) -> Option<std::ops::RangeInclusive<i32>> {
+fn intersect(sensor_beacon: &SensorBeacon, y: i32) -> Option<RangeInclusive<i32>> {
     let distance = manhattan_distance(&sensor_beacon.sensor, &sensor_beacon.beacon);
     if !(sensor_beacon.sensor.y - distance..=sensor_beacon.sensor.y + distance).contains(&y) {
         None
@@ -55,13 +57,65 @@ fn intersect(sensor_beacon: &SensorBeacon, y: i32) -> Option<std::ops::RangeIncl
     }
 }
 
-fn process_row(y: i32, sensors_and_beacons: &Vec<SensorBeacon>, max_x: i32) -> Option<Coord> {
-    let mut candidates: Vec<bool> = vec![true; max_x as usize + 1];
-    for sb in sensors_and_beacons {
-        if let Some(range) = intersect(&sb, y) {
-            for x in range.filter(|&x| x >= 0 && x <= max_x) {
-                candidates[x as usize] = false;
+fn sort_ranges(ranges: &mut Vec<RangeInclusive<i32>>) {
+    ranges.sort_by(|a, b| {
+        if a.start() < b.start() {
+            Ordering::Less
+        } else if a.start() > b.start() {
+            Ordering::Greater
+        } else {
+            if a.end() < b.end() {
+                Ordering::Less
+            } else if a.end() > b.end() {
+                Ordering::Greater
+            } else {
+                Ordering::Equal
             }
+        }
+    });
+}
+
+fn merge_ranges(ranges: &Vec<RangeInclusive<i32>>) -> Vec<RangeInclusive<i32>> {
+    let mut merged = Vec::new();
+    let mut cur_start = *ranges[0].start();
+    let mut max_end = *ranges[0].end();
+    for rng in ranges.iter().skip(1) {
+        if *rng.start() <= max_end + 1 && *rng.end() > max_end {
+            max_end = *rng.end();
+        } else if *rng.start() > max_end + 1 {
+            merged.push(cur_start..=max_end);
+            cur_start = *rng.start();
+            max_end = *rng.end();
+        }
+    }
+    merged.push(cur_start..=max_end);
+    merged
+}
+
+fn process_row(y: i32, sensors_and_beacons: &Vec<SensorBeacon>, max_x: i32) -> Option<Coord> {
+    let mut intersections: Vec<_> = sensors_and_beacons
+        .iter()
+        .filter_map(|sb| intersect(sb, y))
+        .collect();
+
+    if intersections.is_empty() {
+        return None;
+    }
+
+    sort_ranges(&mut intersections);
+    let merged = merge_ranges(&intersections);
+
+    let mut candidates: HashSet<i32> = HashSet::new();
+    if merged[0].start() > &0 {
+        candidates.extend(0..=*merged[0].start() - 1);
+    }
+    if merged.last().unwrap().end() < &max_x {
+        candidates.extend(*merged[0].end() + 1..max_x);
+    }
+
+    for (prev, next) in merged.iter().tuple_windows() {
+        if prev.end() + 1 < *next.start() {
+            candidates.extend(*prev.end() + 1..=*next.start() - 1)
         }
     }
 
@@ -72,23 +126,22 @@ fn process_row(y: i32, sensors_and_beacons: &Vec<SensorBeacon>, max_x: i32) -> O
             None
         }
     }) {
-        candidates[x as usize] = false;
+        candidates.remove(&x);
     }
 
-    let x_pos = candidates
-        .iter()
-        .enumerate()
-        .find(|(_, &x)| x == true)
-        .map(|(i, _)| i as i32);
-
-    if let Some(x) = x_pos {
-        Some(Coord { x, y })
-    } else {
+    if candidates.len() > 0 {
+        Some(Coord {
+            x: candidates.iter().next().unwrap().clone(),
+            y,
+        })
+    } else if candidates.is_empty() {
         None
+    } else {
+        panic!("More than 1 candidate found!")
     }
 }
 
-fn process_range(from: i32, to: i32, tqdm_pos: u16) {
+fn main() {
     let arg = std::env::args().nth(1).unwrap();
     let file_content = std::fs::read_to_string(arg).unwrap();
     let sensors_and_beacons: Vec<SensorBeacon> = file_content
@@ -96,7 +149,7 @@ fn process_range(from: i32, to: i32, tqdm_pos: u16) {
         .filter_map(|line| line.parse().ok())
         .collect();
 
-    for y in tqdm!(from..to, position = tqdm_pos) {
+    for y in 0..=4_000_000 {
         if let Some(coord) = process_row(y, &sensors_and_beacons, 4_000_000) {
             println!(
                 "({},{}) -> {}",
@@ -104,23 +157,7 @@ fn process_range(from: i32, to: i32, tqdm_pos: u16) {
                 coord.y,
                 coord.x as i64 * 4_000_000 + coord.y as i64
             );
+            break;
         }
-    }
-}
-
-fn main() {
-    let handles = [
-        std::thread::spawn(|| process_range(0, 500_000, 0)),
-        std::thread::spawn(|| process_range(500_000, 1_000_000, 1)),
-        std::thread::spawn(|| process_range(1_000_000, 1_500_000, 2)),
-        std::thread::spawn(|| process_range(1_500_000, 2_000_000, 3)),
-        std::thread::spawn(|| process_range(2_000_000, 2_500_000, 4)),
-        std::thread::spawn(|| process_range(2_500_000, 3_000_000, 5)),
-        std::thread::spawn(|| process_range(3_000_000, 3_500_000, 6)),
-        std::thread::spawn(|| process_range(3_500_000, 4_000_001, 7)),
-    ];
-
-    for h in handles {
-        h.join().unwrap();
     }
 }
